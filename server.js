bf980805bb9434ba9d3e25ff0ee9854f75e2d8fd
unknown <mati.luzuriaga@gmail.com>
2025-04-27 15:06:50 -0300

    asd

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
-var express = require('express');
-var app = express();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+
+
+const cors = require('cors');
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
+
+app.use(express.json());
+app.use(bodyParser.json());
+
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
