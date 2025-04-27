923fcb8f323715d6696ec210d3196439e387d91f
unknown <mati.luzuriaga@gmail.com>
2025-04-27 15:23:30 -0300

    Fix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 
 const cors = require('cors');
 const corsOptions = {
-    origin: '*',
+    origin: 'https://mluzuriaga.github.io/',
     credentials: true,
     optionSuccessStatus: 200
 }
